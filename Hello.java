7742e4043551f378adb1285fe5f92f5bc000d4b7
Sharif <sharif.sdsl@gmail.com>
2020-10-30 19:20:03 +0600

    Added a new method greeting to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,11 @@
 public class Hello{
     public static void main(String[] args){
     System.out.println("Vscode is Aswame!!");
+    greeting("Sharif");
 
+    }
+
+    public static void greeting(string name){
+        System.out.println("Welcome " + name);
     }
 }
